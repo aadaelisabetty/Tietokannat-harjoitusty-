ee49efe03e07fd380b61027baf29673431ee8992
Aada Riihimaki <aada.riihi@gmail.com>
2026-03-13 14:55:16 +0200

    Kaikki tallennettu ja TIM-vaatimukset tarkistettu

--- data_ja_kyselyt.sql
+++ data_ja_kyselyt.sql
@@ -1,14 +1,24 @@
--- Vaihe 4: Mallidatan lisääminen
+-- Vaihe 4 Mallidatan lisääminen
 INSERT INTO Kategoria (nimi) VALUES ('Sci-Fi'), ('Komedia'), ('Draama');
 INSERT INTO Elokuva (nimi, julkaisuvuosi, kategoria_id) VALUES ('Inception', 2010, 1), ('Interstellar', 2014, 1), ('The Big Lebowski', 1998, 2);
-INSERT INTO Asiakas (etunimi, sukunimi, sahkoposti) VALUES ('Matti', 'Meikäläinen', 'matti@esimerkki.fi');
+-- Hae elokuvien määrä per kategoria 
+SELECT Kategoria.nimi, COUNT(Elokuva.elokuva_id) AS elokuvien_maara
+FROM Kategoria
+LEFT JOIN Elokuva ON Kategoria.kategoria_id = Elokuva.kategoria_id
+GROUP BY Kategoria.nimi;
+
+-- Aktiivisimmat asiakkaat (Kuka on vuokrannut eniten)
+SELECT Asiakas.etunimi, Asiakas.sukunimi, COUNT(Vuokraus.vuokraus_id) AS vuokraukset
+FROM Asiakas
+LEFT JOIN Vuokraus ON Asiakas.asiakas_id = Vuokraus.asiakas_id
+GROUP BY Asiakas.asiakas_id
+ORDER BY vuokraukset DESC;INSERT INTO Asiakas (etunimi, sukunimi, sahkoposti) VALUES ('Matti', 'Meikäläinen', 'matti@esimerkki.fi');
 INSERT INTO Vuokraus (asiakas_id) VALUES (1);
 INSERT INTO Vuokrausrivi (vuokraus_id, elokuva_id, palautuspvm) VALUES (1, 1, '2026-03-20'), (1, 3, '2026-03-20');
 
--- Vaihe 5: Esimerkkikyselyt
 -- Hae kaikki elokuvat ja niiden kategoriat
 SELECT Elokuva.nimi, Kategoria.nimi AS tyylilaji 
-FROM Elokuva 
+FROM Elokuv
 JOIN Kategoria ON Elokuva.kategoria_id = Kategoria.kategoria_id;
 
 -- Hae asiakkaan vuokraukset
@@ -16,4 +26,4 @@ SELECT Asiakas.etunimi, Elokuva.nimi
 FROM Asiakas 
 JOIN Vuokraus ON Asiakas.asiakas_id = Vuokraus.asiakas_id
 JOIN Vuokrausrivi ON Vuokraus.vuokraus_id = Vuokrausrivi.vuokraus_id
-JOIN Elokuva ON Vuokrausrivi.elokuva_id = Elokuva.elokuva_id;
+JOIN Elokuva ON Vuokrausrivi.elokuva_id = Elokuva.elokuva_id
